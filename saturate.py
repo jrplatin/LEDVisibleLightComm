a3af75fbe00daaeb07864a49cf2e5a760781eb31
Christopher Ng <chris@christopherng.org>
2019-11-15 18:53:02 +0100

    WIP: sort of working????

--- saturate.py
+++ saturate.py
@@ -34,12 +34,8 @@ def tSend():
         if rdytosend:
             ser.write(_to_sending_string("A"*payloadsize).rstrip().encode("utf-8"))
             ser.write(b'\n')
-            mutex.acquire()
-            try:
-                rdytosend = False
-            finally:
-                mutex.release()
-        time.sleep(0.01)
+            rdytosend = False
+        time.sleep(0.1)
 
 def _to_sending_string(str_to_send):
     return "m[" + str_to_send + "\0," + destaddr + "]"
@@ -47,20 +43,24 @@ def _to_sending_string(str_to_send):
 
 def tRecv():
     global rdytosend
+    count = 0
+    oldtime = 0
     while not exitflag:
         try:
             msg = ser.readline().decode("utf-8").rstrip()
             if msg != "" and (msg.startswith("m[R,D")):
-                # print(" >> " + msg)
                 print(msg.split(",")[2][:-1])
             elif msg != "" and debugflag:
-                print(msg)
                 if msg == "m[D]":
-                    mutex.acquire()
-                    try:
-                        rdytosend = True
-                    finally:
-                        mutex.release()
+                    rdytosend = True
+                elif msg.startswith("s[T"):
+                    # oldtime = int(msg.split(",")[-1][:-1])
+                    oldtime = int(round(time.time() * 1000))
+                elif msg.startswith("s[R"):
+                    # newtime = int(msg.split(",")[-1][:-1])
+                    newtime = int(round(time.time() * 1000))
+                    print(str(count) + " " + str(newtime - oldtime))
+                    count += 1
         except serial.SerialException:
             continue
 
